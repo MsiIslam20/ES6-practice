dba280a051d637260fffc525874693999cac6ac9
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-08-06 21:38:23 +0600

    scripts add

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 //Template String
-
 const firstName = "Mahin"
 const lastName = "Ahmed"
-
 // const fullName = firstName + lastName;
 const fullName = `${firstName} ${lastName} is a good boy.`
 console.log(fullName);
+
+//Multiline string
+const paragraph = `I love my country.
+I will be a creative developer.`
+console.log(paragraph);
+
