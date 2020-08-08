dbdd36ab98705e0a34be63e0cb3f395a894e8fa8
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-08-08 20:37:43 +0600

    updates

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Template String
+/*  ========== Template String ===============  **/
 const firstName = "Mahin"
 const lastName = "Ahmed"
 // const fullName = firstName + lastName;
@@ -11,7 +11,7 @@ I will be a creative developer.`
 console.log(paragraph);
 
 
-//Arrow Function
+/*  ========= Arrow function ===============  **/
 
 //multiline
 const doMath = (x , y) => {
@@ -23,5 +23,38 @@ const doMath = (x , y) => {
 // single line
 const doubleIt = num => num * 2;
 const sum = (x , y) => x + y;
+
+/*  ========== Spread Operators ===============  **/
+
+const age = [12, 14, 15, 18, 20]
+const age2 = [22, 44, 25, 38, 10]
+
+// const add = age.concat(age2);
+const result = [...age,...age2]
+// console.log(result);
+
+const arrayMax = [650, 300, 800, 200];
+const max = Math.max(...arrayMax);
+// console.log(max);
+
+
+/*  ========== Class Constructor ===============  **/
+
+class Student{
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+        this.school = "Khanihati ML High School"
+    }
+}
+
+const studentOne = new Student(2, "Saidul");
+const studentTwo = new Student(3, "Islam");
+// console.log(studentOne);
+// console.log(studentTwo);
+
+
+
+
 
 
