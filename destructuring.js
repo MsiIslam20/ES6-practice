037511c4972e2a8716a5ba21ac96a90674d8e563
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-08-09 21:04:14 +0600

    scripts

--- destructuring.js
+++ destructuring.js
@@ -1,3 +1,15 @@
+/*  ========== destructuring ===============  **/
+
+let person = {
+    name : "saidul",
+    age : 20,
+    phone : 2202202,
+    city : "sylhet"
+}
+// When destructuring, you can set a value for one variable to be the value of another variable.
+const {name, nickname = name} = person
+// console.log(name);
+// console.log(nickname);
 
 
 
