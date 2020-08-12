3c1aa42936250947ea4c34f3d4c7fc99826b4fd6
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-08-12 16:04:29 +0600

    changes

--- js-interview-questions/break-continue.js
+++ js-interview-questions/break-continue.js
@@ -1,2 +1,23 @@
 //Using Break
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20, 44, 26];
+
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    if(numbers[i] > 4){
+        break;
+    }
+    // console.log(element);
+}
+
+
+//Using Continue
+const numbersTwo = [1, -2, 3, 4,- 5, 6, 7, 8, -9, 20, 44, -26];
+
+for (let i = 0; i < numbersTwo.length; i++) {
+    const element = numbersTwo[i];
+    if(numbersTwo[i] < 0){
+        continue;
+    }
+    console.log(element);
+}
 
